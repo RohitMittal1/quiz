662bb02dc7f6be544e76cb7993b9f14e2657a0f0
Rohit454 <148869867+RohitMittal1@users.noreply.github.com>
2024-10-24 17:45:53 +0530

    Add files via upload

---
+++ Quiz management system/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  // Each object within the array represents one quiz question with properties like numb (question number), question (text), answer (correct answer), and options (list of choices).
+  {
+    numb: 1,
+    question: "Which planet is called the Red Planet?",
+    answer: "B. MARS",
+    options: [
+      "A. EARTH",
+      "B. MARS",
+      "C. JUPITER",
+      "D. VENUS"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is full of holes but still holds water?",
+    answer: "C. SPONGE",
+    options: [
+      "A. BUCKET",
+      "B. PLATE",
+      "C. SPONGE",
+      "D. GALORE"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What can you catch but not throw?",
+    answer: "B. COLD",
+    options: [
+      "A. BALL",
+      "B. COLD",
+      "C. BIRD",
+      "D. TENNIS"
+    ]
+  },
+    {
+    numb: 4,
+    question: "I belong to you but others use me more than you do. What am I?",
+    answer: "D. NAME",
+    options: [
+      "A. BRAIN",
+      "B. SHADOW",
+      "C. HAND",
+      "D. NAME"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What goes up and down but doesn't move?",
+    answer: "B. ROAD",
+    options: [
+      "A. WATER",
+      "B. ROAD",
+      "C. CLOUDS",
+      "D. WIND"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Tools Making Logerithm",
+      "B. Hyper Text Making Language",
+      "C. Hyper Text Markup Language",
+      "D. Helping Tools Making Language"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the largest planet in our solar system?",
+    answer: "C. Jupiter",
+    options: [
+      "A. Venus",
+      "B. Earth",
+      "C. Jupiter",
+      "D. Ureanus"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who is known as the 'Father of Modern Physics'?",
+    answer: "C. Albert Einstein",
+    options: [
+      "A. Isaac Newton",
+      "B. Galileo Galilei",
+      "C. Albert Einstein",
+      "D. Nikola Tesla",
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the capital of Australia?",
+    answer:"C. Canberra",
+    options: [
+      "A. Sydney",
+      "B. Melbourne",
+      "C. Canberra",
+      "D. Brisbane",
+    ]
+  },
+  {
+    numb: 10,
+    question: "In which year did Christopher Columbus first reach the Americas?",
+    answer: "B. 1492",
+    options: [
+      "A. 1401",
+      "B. 1492",
+      "C. 1520",
+      "D. 1607"
+    ]
+  },
+];
